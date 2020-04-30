754ff513121f4936121b9d587c34b42eaefc314c
Alper Mencek <alpmencek@gmail.com>
2020-04-29 21:30:24 -0700

    Added a function to get specific cities from cities Model

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ Simple JSON file with the collection of global cities:
 https://github.com/lutangar/cities.json
 */
 const cities = require('cities.json');
-
+console.log(getCity("Lyon"))
 // viewed at http://localhost:3000
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/api/jfile',(req,res) =>{
-    res.json(cities[0].name);
-})
+    res.json(getCity("Lyon"));
+});
+function getCity(cityName){
+    for(let index in cities){
+        let current = cities[index];
+       
+        if(current['name'] === cityName){
+          
+            return  cities[index];
+        }
+    }
+}
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
