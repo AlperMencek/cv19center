f1bb12ebfa8ceee78c8de04578931d00aaca3e89
Alper Mencek <alpmencek@gmail.com>
2020-04-29 21:09:36 -0700

    Added cities Json and tested call

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 var path = require('path');
-const cities = require('./Model/Jfile');
+/*
+Simple JSON file with the collection of global cities:
+https://github.com/lutangar/cities.json
+*/
+const cities = require('cities.json');
 
 // viewed at http://localhost:3000
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/api/jfile',(req,res) =>{
-    res.json(cities);
+    res.json(cities[0].name);
 })
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
