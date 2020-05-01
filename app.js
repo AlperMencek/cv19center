28529fb1c6f0e95f4ab6330f58be1ff6292c510f
Alper Mencek <alpmencek@gmail.com>
2020-04-30 17:31:33 -0700

    Created middleware as well as made progress on api calls

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
 var path = require('path');
+const log = require('./middleware/log');
 /*
 Simple JSON file with the collection of global cities:
 https://github.com/lutangar/cities.json
 */
 const cities = require('cities.json');
 console.log(getCity("Lyon"))
+
+
+//init middleware
+app.use(log);
+
 // viewed at http://localhost:3000
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/api/jfile',(req,res) =>{
     res.json(getCity("Lyon"));
 });
+
+app.get('/api/:id',(req,res) =>{
+    const exists =  cities.some(cities => cities.name === req.params.id);
+    if(exists){
+    res.json(cities.filter(cities => cities.name === req.params.id));
+    }else{
+        res.status(400).json({msg:`Location ${req.params.id}, could not be found`});
+    }
+});
+
+//function to recieve the data from a specific city from the city json
 function getCity(cityName){
     for(let index in cities){
         let current = cities[index];
