1e14e03afa5a5d0cee9a6c5d22fe118652334ef8
Alper Mencek <alpmencek@gmail.com>
2020-05-03 01:01:23 -0700

    update

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
+import {NativeSelect, FormControl} from '@material-ui/core';
 
+import {countries} from '../../api';
 
-const SearchBar = () => {
+import styles from './searchBar.module.css';
+//changes only when etfetchedcountries change or when another country is selected.
+const SearchBar = ({handleCountryChange}) => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+    useEffect(() =>{
+        const fetchCountries = async () =>{
+            setFetchedCountries(await countries());
+            
+        }
+        fetchCountries();
+    }, [setFetchedCountries]);
+    console.log(fetchedCountries)
     return(
-        <h1>Search bar</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+                <option value ='global'>Global</option>
+                {fetchedCountries.map((country,i)=> <option key = {i} value={country} >{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
