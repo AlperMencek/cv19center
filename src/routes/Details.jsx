6ac2bc0506e322e65bb8ccc78f1c3f07ffc91c71
Alper Mencek <alpmencek@gmail.com>
2020-05-07 14:29:17 -0700

    Added basic routing and an details page

---
+++ src/routes/Details.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Cards, GlobalCard, Charts, SearchBar, Standings} from '../components';
+
+const Details = () =>{
+    return(
+        
+        <div>
+            <h1>DETAILSPAGE</h1>
+        </div>
+     
+    )
+}
+
+export default Details;
