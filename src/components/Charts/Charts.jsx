45c6b26d783b096293121ae87a4b3e58171cdd2a
Alper Mencek <alpmencek@gmail.com>
2020-05-06 21:36:25 -0700

    Updated media query for chart and major readability updates

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -17,10 +17,58 @@ const Charts = () => {
         dailyData.length?(<Line
         data={{
         labels: dailyData.map(({date})=>date),
-        datasets:[{data: dailyData.map(({confirmed})=>confirmed),label: "Infected",
-    borderColor: '#3333ff',fill: true}, {data: dailyData.map(({deaths})=>deaths),label: "Deaths",
-    borderColor: 'red',backgroundColor:'rgba(255,0,0,.5)' ,fill: true}],
-        }}/>) : null
+        datasets:[
+            {
+            data: dailyData.map(({confirmed})=>confirmed),
+            label: "Confirmed",
+            borderColor: 'rgb(255, 255, 255)',
+            fill: true,
+            backgroundColor:'rgb(255, 255, 255,.2)',
+        }, 
+        {
+            data: dailyData.map(({deaths})=>deaths),
+            label: "Deaths",
+            borderColor: 'rgb(218, 5, 86)',
+            backgroundColor:'rgb(218, 5, 86,.7)' ,
+            fill: true
+        }],
+        }} options = {{
+            responsive:true, 
+            maintainAspectRatio: false,
+            title:{text:"World Data",display:true, fontColor:'white',fontSize: 30, },
+            scales:{
+                yAxes:[
+                    {
+                        ticks:{
+                                fontColor: 'white',
+                                maxTicksLimit:10,
+                                fontSize: 13,
+                        },
+                       gridLines:{
+                           
+                       },
+                       
+                }
+            ],
+        xAxes:[
+            {
+                ticks:{
+                    fontColor: 'white',
+                    fontSize: 11,
+            },
+           gridLines:{
+               
+           },   
+            }
+        ]},  
+            
+            legend:{
+                
+                labels:{
+                    fontColor:'white'
+                }
+            }
+        }} />) : null
     )
     
     return(
