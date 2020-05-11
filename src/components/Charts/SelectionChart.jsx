1b8593669ac0e5ac01f71f7e58d5bc9642afac65
wang <sujae815@yandex.com>
2020-05-11 18:38:21 +0200

    detailsView api issue - step1

--- src/components/Charts/SelectionChart.jsx
+++ src/components/Charts/SelectionChart.jsx
@@ -1,87 +1,82 @@
 import React, { useState, useEffect } from 'react';
-import {fetchc19DailyData} from '../../api';
-import { Line, Bar} from 'react-chartjs-2';
+import { fetchc19DailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
 import styles from './Charts.module.css';
-const SelectionChart = ({ slug:slugTitle}) => { 
-  
+const SelectionChart = ({ slug: slugTitle }) => {
+
     const [dailyData, setDailyData] = useState([]);
-    
-    useEffect(()=> { 
-        
-       console.log(slugTitle)
+
+    useEffect(() => {
+
+        console.log(slugTitle)
         const fetchAPI = async () => {
             console.log("within fetchAPI")
             setDailyData(await fetchc19DailyData(slugTitle));
         }
-      
         fetchAPI();
-    
-    },[slugTitle]);
-  
+    }, [slugTitle]);
 
- 
-
-    
     const lineChart = (
-        dailyData.length?(<Line
-        data={{ 
-        labels: dailyData.map(({date})=>date),
-        datasets:[
-            {
-            data: dailyData.map(({Confirmed})=>Confirmed),
-            label: "Confirmed",
-            borderColor: 'rgb(255, 255, 255)',
-            fill: true,
-            backgroundColor:'rgb(255, 255, 255,.2)',
-        }, 
-        {
-            data: dailyData.map(({Deaths})=>Deaths),
-            label: "Deaths",
-            borderColor: 'rgb(218, 5, 86)',
-            backgroundColor:'rgb(218, 5, 86,.7)' ,
-            fill: true
-        }],
-        }} options = {{
-            responsive:true, 
-            maintainAspectRatio: false,
-            title:{text:"World Data",display:true, fontColor:'white',fontSize: 30, },
-            scales:{
-                yAxes:[
+        dailyData.length ? (<Line
+            data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [
                     {
-                        ticks:{
+                        data: dailyData.map(({ Confirmed }) => Confirmed),
+                        label: "Confirmed",
+                        borderColor: 'rgb(255, 255, 255)',
+                        fill: true,
+                        backgroundColor: 'rgb(255, 255, 255,.2)',
+                    },
+                    {
+                        data: dailyData.map(({ Deaths }) => Deaths),
+                        label: "Deaths",
+                        borderColor: 'rgb(218, 5, 86)',
+                        backgroundColor: 'rgb(218, 5, 86,.7)',
+                        fill: true
+                    }],
+            }} options={{
+                responsive: true,
+                maintainAspectRatio: false,
+                title: { text: "World Data", display: true, fontColor: 'white', fontSize: 30, },
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
                                 fontColor: 'white',
-                                maxTicksLimit:10,
+                                maxTicksLimit: 10,
                                 fontSize: 13,
-                        },
-                       gridLines:{
-                           
-                       },
-                       
+                            },
+                            gridLines: {
+
+                            },
+
+                        }
+                    ],
+                    xAxes: [
+                        {
+                            ticks: {
+                                fontColor: 'white',
+                                fontSize: 11,
+                            },
+                            gridLines: {
+
+                            },
+                        }
+                    ]
+                },
+
+                legend: {
+
+                    labels: {
+                        fontColor: 'white'
+                    }
                 }
-            ],
-        xAxes:[
-            {
-                ticks:{
-                    fontColor: 'white',
-                    fontSize: 11,
-            },
-           gridLines:{
-               
-           },   
-            }
-        ]},  
-            
-            legend:{
-                
-                labels:{
-                    fontColor:'white'
-                }
-            }
-        }} />) : null
+            }} />) : null
     )
     console.log(dailyData);
 
-    return(
+    return (
         <div className={styles.container}>
             {lineChart}
         </div>
--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -4,42 +4,49 @@ import {
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {DetailsCard,SelectionChart} from '../components';
+import { DetailsCard, SelectionChart } from '../components';
 import styles from './details.module.css'
-import {fetchData , fetchSumData} from '../api';
+import { fetchData, fetchSumData } from '../api';
 
 
-import {Cards, GlobalCard, Charts, SearchBar, Standings} from '../components';
+import { Cards, GlobalCard, Charts, SearchBar, Standings } from '../components';
 
-class Details extends React.Component{
+class Details extends React.Component {
     state = {
-        selectionTitle: '', 
+        selectionTitle: '',
         slugTitle: '',
-        c19Data:{},
-        
+        c19Data: {},
+
     }
-    async componentDidMount(){
-        const {handle} = this.props.match.params;
-        const {title} = this.props.location.state;
-        const {slug} = this.props.location.state;
-        
-        const fetchRes = await fetchSumData();   
-        
-        
-        this.setState({selectionTitle:title , slugTitle:slug , c19Data: fetchRes})
+    async componentDidMount() {
+        const { handle } = this.props.match.params;
+        const { title } = this.props.location.state;
+        const { slug } = this.props.location.state;
+        const fetchRes = await fetchSumData();
+        this.setState({ selectionTitle: title, slugTitle: slug, c19Data: fetchRes })
     }
-    render(){
-     const {selectionTitle , slugTitle, c19Data} = this.state
-  
-    return(
-        <div className={styles.container}>
-            <DetailsCard sTitle={selectionTitle} slug={slugTitle} c19Data={c19Data} />
-            <SelectionChart slug={slugTitle}> </SelectionChart>
-        </div>
-     
+
+    render() {
+        const { selectionTitle, slugTitle, c19Data } = this.state
+
+        if(this.props.location.state && this.props.location.state.slug){
+            const slug = this.props.location.state.slug;
+            return(
+                <div className={styles.container}>
+                    <DetailsCard sTitle={selectionTitle} slug={slug} c19Data={c19Data} />
+                    <SelectionChart slug={slug}> </SelectionChart>
+                </div>
+            )
+        }
+        return(
+            <div className={styles.container}>
+                Loading...
+            </div>
         )
+
+
     }
 }
 
